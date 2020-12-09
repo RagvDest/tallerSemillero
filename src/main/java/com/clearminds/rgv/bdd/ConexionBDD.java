53f7952617bb830dd7ff7641bdc25e4d80ba213d
= <rennygorozabel@gmail.com>
2020-12-09 10:31:00 -0500

    update leerPropiedad method System.out

--- src/main/java/com/clearminds/rgv/bdd/ConexionBDD.java
+++ src/main/java/com/clearminds/rgv/bdd/ConexionBDD.java
@@ -10,11 +10,11 @@ import com.clearminds.rgv.excepciones.BDDException;
 
 public class ConexionBDD {
 
-	public static String leerPropiedad(String prop){
+	private static String leerPropiedad(String prop){
 		Properties p = new Properties();
 		try {
 			p.load(new FileReader("conexion.properties"));
-			System.out.println("Uno: "+p.getProperty(prop));
+			System.out.println(prop+": "+p.getProperty(prop));
 			return p.getProperty(prop);
 		} catch (Exception e) {
 			return null;		
